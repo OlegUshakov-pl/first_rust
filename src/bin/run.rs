01c2cb0cd0a99a2c78c55e88fb6745bdc128ee45
Oleg Ushakov <olegushakov38@gmail.com>
2026-08-03 06:45:21 +0200

    Update run.rs

--- src/bin/run.rs
+++ src/bin/run.rs
@@ -1,10 +1,11 @@
-
+const pi:f64=std::f64::consts::PI;
 
 
 fn main() {
-    let pi=std::f64::consts::PI;
     let s: &str = "hello"; // &str
     let s: String = s.to_uppercase(); // String
     println!("{s}"); // HELLO
     println!("{}",pi );
+
+    
 }
